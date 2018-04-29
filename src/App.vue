9b7126a134093cac2889a60858109f37d9ab6d7b
MrAnsheff <mransheff86@gmail.com>
2018-04-29 07:25:55 +0400

    Communicate components

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,26 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <app-servers></app-servers>
-            <app-server-details></app-server-details>
-
+            <div class="col-xs-12">
+                <app-user></app-user>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
-import Servers from './components/Servers.vue'
-import ServerDetails from './components/ServerDetails.vue'
-export default {
-    components:{
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-servers': Servers,
-    'app-server-details': ServerDetails
-    }
-}
+    import User from './components/User.vue';
 
+    export default {
+        components: {
+            appUser: User
+        }
+    }
 </script>
 
 <style>
-
+    div.component {
+        border: 1px solid black;
+        padding: 30px;
+    }
 </style>
