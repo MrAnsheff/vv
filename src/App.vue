106fa477f017ffc0795ee11f12a45cf5079ddc6c
MrAnsheff <mransheff86@gmail.com>
2018-05-08 15:40:26 +0400

    Ready Quotes App

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <template>
     <div class="container">
+        <app-progress :quoteCount="this.quotes.length" :maxQuotes="maxQuotes"></app-progress>
+        <app-new-qoute @addQuote='newQuote'></app-new-qoute>
+        <app-quotes-grid :quotes='quotes' @deleteQuote='deleteQuote'></app-quotes-grid>
         <div class="row">
-            <div class="col-xs-12">
-                <br>
-                <button class="btn btn-primary" @click="viewC = 'appBlue'">Load Blue Template</button>
-                <button class="btn btn-success" @click="viewC = 'appGreen'">Load Green Template</button>
-                <button class="btn btn-danger" @click="viewC = 'appRed'">Load Red Template</button>
-                <hr>
-                <keep-alive>
-                <component :is="viewC"></component>
-                </keep-alive>
+            <div class="col-sm-12 text-center">
+                <div class="alert alert-info">
+                    Info: Click on a quote to delete it!
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    import Blue from './components/Blue.vue';
-    import Green from './components/Green.vue';
-    import Red from './components/Red.vue';
+    import QuotesGrid from './components/QuotesGrid.vue';
+    import NewQuote from './components/NewQuote.vue'
+    import ProgressBar from './components/ProgressBar.vue'
 
     export default {
-        data: function(){
-           return {
-            viewC:'appBlue'
-           }
+        data: function() {
+            return {
+                quotes:[
+                    'The first simple quote'
+                ],
+                maxQuotes: 10
+            }
         },
-        components: {
-            appBlue: Blue,
-            appGreen: Green,
-            appRed: Red
+        methods:{
+            newQuote(quote){
+                if(this.quotes.length>=10){
+                    return alert('Please delete anyone quote first!');
+                }
+                this.quotes.push(quote)
+            },
+            deleteQuote(index){
+                this.quotes.splice(index,1);
+            }
+
+        },
+        components:{
+            appQuotesGrid: QuotesGrid,
+            appNewQoute: NewQuote,
+            appProgress: ProgressBar
         }
     }
 </script>
---
+++ src/components/QuotesGrid.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row">
+        <new-quote v-for="(q, index) in quotes" :key="q" @click.native="deleteQuote(index)">{{ q }}</new-quote>
+    </div>
+</template>
+
+<script>
+import Quote from './Quote.vue'
+export default {
+    props: ['quotes'],
+    components:{
+        newQuote:Quote
+    },
+    methods: {
+        deleteQuote(index){
+            this.$emit('deleteQuote', index);
+        }
+    }
+  
+}
+</script>
+
+<style>
+
+</style>
